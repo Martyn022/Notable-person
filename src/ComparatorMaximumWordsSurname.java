9beaf6817ce85e2abd27f00f13d7c748cf6509b4
Martyn022 <mg.sharipov@gmail.com>
2023-03-02 20:47:29 +0500

    Исправил Стринг в Человеке, внес данные в Перосон майн.

--- src/ComparatorMaximumWordsSurname.java
+++ src/ComparatorMaximumWordsSurname.java
@@ -1,3 +1,25 @@
-public class ComparatorMaximumWordsSurname {
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class ComparatorMaximumWordsSurname implements Comparator<Person> {
+    private int maxAmountWords;
+
+    public ComparatorMaximumWordsSurname(int maxAmountWords) {
+        this.maxAmountWords = maxAmountWords;
+    }
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        StringTokenizer st1 = new StringTokenizer(o1.getSurname());
+        StringTokenizer st2 = new StringTokenizer(o2.getSurname());
+        if (st1.countTokens() >= maxAmountWords || st2.countTokens() >= maxAmountWords) {
+            if (st1.countTokens() < st2.countTokens()) {
+                return -1;
+            } else if (st1.countTokens() > st2.countTokens()) {
+                return 1;
+            }
+        }
+        return Integer.compare(o1.getAge(), o2.getAge());
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Person> person = new ArrayList<>();
+        person.add(new Person("Benedict", "Timothy Carlton Cumberbatch", 46));
+        person.add(new Person("Morena", "Silva de Vaz Setta Baccarin", 43));
+        person.add(new Person("Benicio", "Monserrate Rafael del Toro Sánchez", 56));
+        person.add(new Person("Mel", "Colmcille Gerard Gibson", 67));
+        person.add(new Person("Emilia", "Isobel Euphemia Rose Clarke", 36));
+        person.add(new Person("Anya-Josephine", "Marie Taylor Joy", 26));
+        Collections.sort(person, new ComparatorMaximumWordsSurname(3));
 
-        System.out.println("Hello world!");
+        System.out.println(person);
     }
+
 }
