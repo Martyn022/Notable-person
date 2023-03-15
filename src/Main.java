d86bbcc09e275475f9df1ec7ee3c2a00871583fd
Martyn022 <mg.sharipov@gmail.com>
2023-03-15 22:33:35 +0500

    Отвел ветку eraser, вписал двух несовершеннолетних, лямбда предикат их удаляет

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> person = new ArrayList<>();
-        person.add(new Person("Benedict", "Timothy Carlton Cumberbatch", 46));
-        person.add(new Person("Morena", "Silva de Vaz Setta Baccarin", 43));
-        person.add(new Person("Benicio", "Monserrate Rafael del Toro Sánchez", 56));
-        person.add(new Person("Mel", "Colmcille Gerard Gibson", 67));
-        person.add(new Person("Emilia", "Isobel Euphemia Rose Clarke", 36));
-        person.add(new Person("Anya-Josephine", "Marie Taylor Joy", 26));
-        Collections.sort(person, new ComparatorMaximumWordsSurname(3));
+        List<Person> actor = new ArrayList<>();
+        actor.add(new Person("Grace", "Mckenna", 16));
+        actor.add(new Person("Benedict", "Timothy Carlton Cumberbatch", 46));
+        actor.add(new Person("Morena", "Silva de Vaz Setta Baccarin", 43));
+        actor.add(new Person("Emilia", "Isobel Euphemia Rose Clarke", 36));
+        actor.add(new Person("Abby", "Ryder Fortson", 15));
+        actor.add(new Person("Anya-Josephine", "Marie Taylor Joy", 26));
 
-        System.out.println(person);
+        Predicate<Person> predicate = (person -> person.getAge() < 18);
+        actor.removeIf(predicate);
+
+        System.out.println(actor);
     }
 
 }
