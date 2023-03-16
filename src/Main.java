b1c7caa57881dd2fdd4cc3001e66eb41eb002bd5
Martyn022 <mg.sharipov@gmail.com>
2023-03-16 22:19:27 +0500

    Удалил инт максимальных слов, иф сравнения его со счетчиком, подправил АррайЛист

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> actor = new ArrayList<>();
+        List<Person> actor = new ArrayList<Person>();
         actor.add(new Person("Benedict", "Timothy Carlton Cumberbatch", 46));
         actor.add(new Person("Morena", "Silva de Vaz Setta Baccarin", 43));
         actor.add(new Person("Benicio", "Monserrate Rafael del Toro Sánchez", 56));
@@ -12,21 +12,18 @@ public class Main {
         actor.add(new Person("Emilia", "Isobel Euphemia Rose Clarke", 36));
         actor.add(new Person("Anya-Josephine", "Marie Taylor Joy", 26));
 
-        List<Person> personList = new ArrayList<>();
-        personList.sort((o1, o2) -> {
+        actor.sort((o1, o2) -> {
             StringTokenizer st1 = new StringTokenizer(o1.getSurname());
             StringTokenizer st2 = new StringTokenizer(o2.getSurname());
-            int maxAmountWords = 0;
-            if (st1.countTokens() >= maxAmountWords || st2.countTokens() >= maxAmountWords) {
                 if (st1.countTokens() < st2.countTokens()) {
                     return -1;
                 } else if (st1.countTokens() > st2.countTokens()) {
                     return 1;
                 }
-            }
             return Integer.compare(o1.getAge(), o2.getAge());
         });
         System.out.println(actor);
+
     }
 
 }
