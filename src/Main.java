e84835886530efe8a0d0c68d51782f16d3596020
Martyn022 <mg.sharipov@gmail.com>
2023-03-16 22:25:06 +0500

    подправил АррайЛист, работает ранжир по возрасту

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> actor = new ArrayList<>();
+        List<Person> actor = new ArrayList<Person>();
         actor.add(new Person("Grace", "Mckenna", 16));
         actor.add(new Person("Benedict", "Timothy Carlton Cumberbatch", 46));
         actor.add(new Person("Morena", "Silva de Vaz Setta Baccarin", 43));
